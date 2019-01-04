69b2901dbd376fc6956778ae8bb1e48dd071282b
Christoffer Niska <christofferniska@gmail.com>
2019-01-04 20:57:35 +0200

    Remove @ts-ignore from parser

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -8,62 +8,66 @@ import {
   IDependency
 } from "./ast";
 
-type ParseFn = (value: string) => any;
+type ParseFn<ParsedValue> = (value: string) => ParsedValue;
 
-export const dependencyParser: ParseFn = value => {
-  return value
-    .split(", ")
-    .reduce((dependencies: IDependency[], current: string): IDependency[] => {
-      const [name, ...alternates] = (current.split(" | ") || [current]).map(
-        part => part.split(" ")[0]
-      );
+export const dependencyParser: ParseFn<IDependency[]> = value => {
+  const dependencyReducer = (
+    dependencies: IDependency[],
+    current: string
+  ): IDependency[] => {
+    const [name, ...alternates] = (current.split(" | ") || [current]).map(
+      part => part.split(" ")[0]
+    );
 
-      return !inArray(
-        dependencies,
-        (dependency: IDependency) => dependency.name === name
-      )
-        ? [...dependencies, { name, alternates }]
-        : dependencies;
-    }, []);
+    return !inArray(
+      dependencies,
+      (dependency: IDependency) => dependency.name === name
+    )
+      ? [...dependencies, { name, alternates }]
+      : dependencies;
+  };
+
+  return value.split(", ").reduce(dependencyReducer, []);
 };
 
-const fieldValueParsers: { [key: string]: ParseFn } = {
+const fieldValueParsers: { [fieldName: string]: ParseFn<any> } = {
   Depends: dependencyParser
 };
 
 export default class Parser {
   public parse = (source: string): IDocumentNode => {
-    const packages = source.split("\n\n").map(this.parsePackage);
+    const packages = source.split("\n\n").map(this._parsePackage);
+
     packages.sort((a, b) => a.package.value.localeCompare(b.package.value));
+
     return {
       kind: NodeKind.Document,
       packages
     };
   };
 
-  private parsePackage = (source: string): IPackageNode => {
+  private _parsePackage = (source: string): IPackageNode => {
     const pattern = /^[\w\-]+: (?:.|\n\s)+$/gm;
-    const fieldData = (source.match(pattern) || []).map(this.parseField);
+    const fieldData = (source.match(pattern) || []).map(this._parseField);
     const fieldReducer = (
-      data: { [key: string]: IFieldNode<any> },
-      fieldName: string
+      data: { [fieldName: string]: IFieldNode<any> },
+      fieldName: FieldName
     ) => {
-      // @ts-ignore
-      const field = this.findField(FieldName[fieldName], fieldData);
+      const field = this._findField(FieldName[fieldName], fieldData);
+
       return field ? { [camelCase(fieldName)]: field, ...data } : data;
     };
-    const fields = Object.values(FieldName).reduce(fieldReducer, {});
 
-    // @ts-ignore
     return {
       kind: NodeKind.Package,
-      ...fields
+      ...Object.values(FieldName).reduce(fieldReducer, {})
     };
   };
 
-  private parseField = (source: string): IFieldNode<any> => {
+  private _parseField = (source: string): IFieldNode<any> => {
     const [fieldName, fieldValue] = source.split(": ");
     const parseFn = fieldValueParsers[fieldName];
+
     return {
       kind: NodeKind.Field,
       name: fieldName,
@@ -71,10 +75,6 @@ export default class Parser {
     };
   };
 
-  private findField = (
-    fieldName: FieldName,
-    fields: Array<IFieldNode<any>>
-  ) => {
-    return (fields.filter(field => field.name === fieldName) || [])[0];
-  };
+  private _findField = (fieldName: FieldName, fields: Array<IFieldNode<any>>) =>
+    (fields.filter(field => field.name === fieldName) || [])[0];
 }
